6ae3fd7e33283739a31eef6c2e2f5c4fd4bf9c09
Jeffr <emanueljeferson@gmail.com>
2026-05-30 18:51:19 -0300

    First test using application with service via middleware

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,6 +3,17 @@ mod applications;
 mod services;
 mod hal;
 
+use middleware::message::Message;
+use middleware::message::Topics;
+use middleware::broker::Broker;
+use applications::app_motor_control::MotorControl;
+use services::rtos::RtosService;
+
 fn main() {
-    println!("Hello, world!");
+    let mut broker = Broker::new();
+
+    RtosService::init(&broker);
+    
+    let app_motor = MotorControl::new("motor_left".to_string());
+    app_motor.init(&broker);
 }
