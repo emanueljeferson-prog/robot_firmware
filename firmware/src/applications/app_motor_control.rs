ddf4f049574f9eda388137332c970699b2203a61
Jeffr <emanueljeferson@gmail.com>
2026-05-30 18:48:51 -0300

    Updating motor control task

---
+++ firmware/src/applications/app_motor_control.rs
@@ -0,0 +1,57 @@
+use crate::middleware::broker::Broker;
+use crate::middleware::message::Message;
+use crate::middleware::message::TaskDescriptor;
+
+use core::ffi::c_void;
+use core::ptr::NonNull;
+
+pub struct MotorControl {
+    signal: f32,
+    speed: f32,
+    motor: String
+}
+
+impl MotorControl {
+    pub fn new(name: String) -> Self {
+        Self { signal: 0.0, speed: 0.0, motor: name }
+    }
+
+    pub fn init(&self, middleware: &Broker) {
+        middleware.publish(
+            &Message::RegisterTask(
+                TaskDescriptor {
+                    name: "motor_control_task",
+                    stack_size: 1024,
+                    priority: 1,
+                    delay_ms: 1000,
+                    task: motor_control_task,
+                    context: Some(NonNull::from(self)
+                        .cast::<c_void>())
+                }
+            )
+        )
+    }
+
+    pub fn control(&self) {
+        println!("Motor control '{}'...", self.motor);
+    }
+
+    pub fn read_speed(&self) {
+        println!("Read speed '{}'...", self.motor);
+    }
+}
+
+fn motor_control_task(
+    ctx: *mut c_void
+)
+{
+    let motor =
+        unsafe {
+            &*(ctx as *const MotorControl)
+        };
+
+    loop {
+        motor.control();
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+}
