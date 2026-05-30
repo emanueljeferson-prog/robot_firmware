a9901fded47ec9268e8905bf267f849589012778
Jeffr <emanueljeferson@gmail.com>
2026-05-30 18:49:33 -0300

    Updating subscriber method

--- firmware/src/middleware/broker.rs
+++ firmware/src/middleware/broker.rs
@@ -2,26 +2,49 @@ use super::message::Subscriber;
 use super::message::Callback;
 use super::message::Topics;
 use super::message::Message;
+use std::sync::Mutex;
 
 pub struct Broker {
-    subscribers: Vec<Subscriber>
+    subscribers: Mutex<Vec<Subscriber>>
 }
 
 impl Broker {
     pub fn new() -> Self {
-        Self { subscribers: Vec::new() }
+        Self { subscribers: Mutex::new(Vec::new()) }
     }
 
-    pub fn subscribe(&mut self, callback: Callback, topic: Topics) {
-        self.subscribers.push(
-            Subscriber::new(callback, topic)
-        );
-    }
-
-    pub fn publish(&self, message: &Message) {
+    pub fn subscribe(&self, callback: Callback, topic: Topics) {
         self.subscribers
-            .iter()                                                         // iterator
-            .filter( |subscriber| { subscriber.topic == message.topic() } ) // filter by equal topic
-            .for_each( |subscriber| { (subscriber.callback)(message) })     // send the message to the subscriber
+            .lock()
+            .unwrap()
+            .push( Subscriber::new(callback, topic) );
+    }
+
+    pub fn publish(
+        &self,
+        message: &Message
+    ) {
+
+        let subscribers =
+            self.subscribers
+                .lock()
+                .unwrap();
+
+        let callbacks = subscribers
+            .iter()
+            .filter(|subscriber| {
+                subscriber.topic ==
+                message.topic()
+            })
+            .map(|subscriber| {
+                subscriber.callback.clone()
+            })
+            .collect::<Vec<_>>();
+
+        drop(subscribers); // unlock explícito
+
+        for callback in callbacks {
+            callback(message);
+        }
     }
 }
